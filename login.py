296318b7b0fbb9cbd3f42f0accfcdc830a8ba7b7
Sourcery AI <>
2022-10-03 04:46:30 +0000

    'Refactored by Sourcery'

--- login.py
+++ login.py
@@ -16,12 +16,11 @@ if option=="signup":
     num="0123456789"
     spch="!@#$%^&"
     if len(password)>=8:
-        i=0
         a=0
         b=0
         c=0
         d=0
-        while i<len(password):
+        for i in range(len(password)):
             if password[i] in cl:
                 a=a+1
             elif password[i] in sl:
@@ -30,45 +29,43 @@ if option=="signup":
                 c=c+1
             elif password[i] in spch:
                 d=d+1
-            i=i+1
         if a>=1 and b>=1 and c>=1 and d>=1:
             print("ur passward is strong")
         try:
             with open("data.json","r") as output:
                 user_data=json.load(output)        
         except:
-            pass 
+            pass
         comform_password=input('enter the comfirm password: ')
-        if password==comform_password:            
-            if  ("data.json"):
-                print("Congractstulations!!!",username,"You are signed up Succesfully")
-                description=input("Enter your Description: ")
-                birth_date=input("enter Your Date Of Birth: ")
-                Gender=input("enter your Gender: ")
-                hobbies=input("Enter Your hobby: ")
-                try:
-                    user_info["description"]=description
-                    user_info["d_o_b"]=birth_date
-                    user_info["Gender"]=Gender
-                    user_info["Hobbies"]=hobbies
-                    dict["Username"]=username
-                    dict["Password"]=password
-                    dict["Profile"]=user_info
-                    list.append(dict)
-                    main_dict["user"]=list
-                    with open("data.json","r+") as file:
-                        read_file= file.read()
-                        userdata=json.loads(read_file)
-                        for i in userdata:
-                            if i =="user":
-                                x=userdata[i]
-                                x.append(dict.copy())
-                                main_dict["user"]=x
-                                json.dumps(main_dict,file)
-                                file.close()
-                except:
-                    with open("data.json","w") as f:
-                        f.write(json.dumps(main_dict, indent=4))
+        if password==comform_password:    
+            print("Congractstulations!!!",username,"You are signed up Succesfully")
+            description=input("Enter your Description: ")
+            birth_date=input("enter Your Date Of Birth: ")
+            Gender=input("enter your Gender: ")
+            hobbies=input("Enter Your hobby: ")
+            try:
+                user_info["description"]=description
+                user_info["d_o_b"]=birth_date
+                user_info["Gender"]=Gender
+                user_info["Hobbies"]=hobbies
+                dict["Username"]=username
+                dict["Password"]=password
+                dict["Profile"]=user_info
+                list.append(dict)
+                main_dict["user"]=list
+                with open("data.json","r+") as file:
+                    read_file= file.read()
+                    userdata=json.loads(read_file)
+                    for i in userdata:
+                        if i =="user":
+                            x=userdata[i]
+                            x.append(dict.copy())
+                            main_dict["user"]=x
+                            json.dumps(main_dict,file)
+                            file.close()
+            except:
+                with open("data.json","w") as f:
+                    f.write(json.dumps(main_dict, indent=4))
         else: 
             print('both passwords are not correct ')
     else:
